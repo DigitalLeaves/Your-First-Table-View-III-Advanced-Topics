2adccf20151b25064864be56905bcf12d0e7edc1
Ignacio Nieto <contact@digitalleaves.com>
2017-05-07 19:38:48 +0200

    Initial scenario

--- YourFirstTableView/ViewController.swift
+++ YourFirstTableView/ViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    // outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    // data
+    let animals = ["dog", "cat", "tiger", "lion", "giraffe", "cattle", "horse", "fox", "panda", "bear", "rat", "pig", "rhinoceros", "hippopotamus", "rabbit", "elephant", "crocodile", "bird", "sheep", "gorilla"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - UITableViewDelegate/DataSource methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myTableViewCell", for: indexPath) 
+        cell.textLabel?.text = animals[indexPath.row]
+        return cell
+    }
 
 }
 
